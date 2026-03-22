4e85ce20d6a311b1c7057dcd1ff5a91d1297f012
Achiver-Abhinav01 <achiver.abhinav01@gmail.com>
2026-03-22 20:54:09 +0530

    add files

---
+++ Off-Campus_prep/Basics-IP&OP/Taking-input.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s; 
+    int a;
+
+    cout << "Enter name and age separated with space: ";
+    cin >> s >> a;
+
+    cout << "Hi " << s << ", you're " << a << " years old. Thanks for your participation";
+
+    return 0;
+}
